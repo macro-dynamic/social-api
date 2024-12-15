7ace578edc90fa744e010c9043b2d2c862cb55ca
macro-dynamic <147334988+macro-dynamic@users.noreply.github.com>
2024-12-15 18:18:50 +0100

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,1 +1,18 @@
+# views.py
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import User, Transaction
+from .serializers import UserSerializer, TransactionSerializer
+
+class UserListView(APIView):
+    def get(self, request):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+class TransactionListView(APIView):
+    def get(self, request):
+        transactions = Transaction.objects.all()
+        serializer = TransactionSerializer(transactions, many=True)
+        return Response(serializer.data)
 
