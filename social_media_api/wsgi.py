8425788d54f052709f5ed94df5dcfc4a7ef91f94
macro-dynamic <147334988+macro-dynamic@users.noreply.github.com>
2024-12-15 18:12:55 +0100

    Update wsgi.py

--- social_media_api/wsgi.py
+++ social_media_api/wsgi.py
@@ -1,1 +1,8 @@
+# wsgi.py
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_media_api.settings')
+
+application = get_wsgi_application()
 
