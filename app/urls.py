1703a820aaf519dc1666e1943e5bb6d61526da76
macro-dynamic <147334988+macro-dynamic@users.noreply.github.com>
2024-12-15 18:18:31 +0100

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,1 +1,9 @@
+# urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('users/', views.UserListView.as_view(), name='user-list'),
+    path('transactions/', views.TransactionListView.as_view(), name='transaction-list'),
+]
 
